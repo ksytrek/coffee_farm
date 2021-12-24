66d71c0c5b25ae26c9a436f09e80b73f5d19aa4b
SOMPHOL WILA <sompholwilaza101@gmail.com>
2021-12-24 15:51:40 +0700

    account

---
+++ view/roasters/shop-account.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<!--
+Template: Metronic Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 3.3.4
+Version: 1.0.0
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+-->
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<!-- Head BEGIN -->
+
+<head>
+    <meta charset="utf-8">
+    <title>My Account | Metronic Shop UI</title>
+
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <meta content="Metronic Shop UI description" name="description">
+    <meta content="Metronic Shop UI keywords" name="keywords">
+    <meta content="keenthemes" name="author">
+
+    <meta property="og:site_name" content="-CUSTOMER VALUE-">
+    <meta property="og:title" content="-CUSTOMER VALUE-">
+    <meta property="og:description" content="-CUSTOMER VALUE-">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="-CUSTOMER VALUE-"><!-- link to image for socio -->
+    <meta property="og:url" content="-CUSTOMER VALUE-">
+
+    
+
+</head>
+<!-- Head END -->
+<?php require_once('./navbar.php')?>
+<!-- Body BEGIN -->
+
+<body class="ecommerce">
+
+
+    <div class="main">
+        <div class="container">
+            <ul class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="">Store</a></li>
+                <li class="active">My Account Page</li>
+            </ul>
+
+            <div class="row margin-bottom-40">
+
+                <!-- <div class="sidebar col-md-3 col-sm-3">
+                    <ul class="list-group margin-bottom-25 sidebar-menu">
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> Login/Register</a></li>
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> Restore Password</a></li>
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> My account</a></li>
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> Address book</a></li>
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> Wish list</a></li>
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> Returns</a></li>
+                        <li class="list-group-item clearfix"><a href="javascript:;"><i class="fa fa-angle-right"></i> Newsletter</a></li>
+                    </ul>
+                </div> -->
+
+                <div class="col-md-12 col-sm-12">
+                    <h1>My Account Page</h1>
+                    <div class="content-page">
+                        <h3>My Account</h3>
+                        <ul>
+                            <li><a href="javascript:;">Edit your account information</a></li>
+                            <li><a href="javascript:;">Change your password</a></li>
+                            <li><a href="javascript:;">Modify your address book entries</a></li>
+                            <li><a href="javascript:;">Modify your wish list</a></li>
+                        </ul>
+                        <hr>
+
+                        <h3>My Orders</h3>
+                        <ul>
+                            <li><a href="javascript:;">View your order history</a></li>
+                            <li><a href="javascript:;">Downloads</a></li>
+                            <li><a href="javascript:;">Your Reward Points</a></li>
+                            <li><a href="javascript:;">View your return requests</a></li>
+                            <li><a href="javascript:;">Your Transactions</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- END CONTENT -->
+            </div>
+            <!-- END SIDEBAR & CONTENT -->
+        </div>
+    </div>
+
+    <?php require_once('./footer.php')?>
+  </body>
+<!-- END BODY -->
+
+</html>
